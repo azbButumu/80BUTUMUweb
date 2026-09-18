3bc75c1a642f60eb659df9ebbfd4adcf6dead410
azbButumu <eitakurosawa@icloud.com>
2026-09-18 16:15:59 +0900

    Initial commit

---
+++ 80ButumuWeb/src/Tab.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Tab() {
+  return (
+    <main className='Tab'>
+        <h2>物理部展</h2>
+        <ul>
+            <li>Room301</li>
+            <li>Room302</li>
+        </ul>
+    </main>
+  )
+}
